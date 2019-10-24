c4e443c120c90e9cfd0f7dff793aff6243968137
Destructive code <rulitosxd1@gmail.com>
2019-10-24 13:01:56 -0500

    Add files via upload

---
+++ 2nd Simulation/sim2.py
@@ -0,0 +1,34 @@
+class Disk:
+    self.MASS
+    self.RADIUS
+    self.COLOR  # Tupla 3 elementos RGB
+    self.TAG    # Nombre de la particula || Rotula
+
+    # np.array
+    # posiciones y velocidades x,y
+    self.x, self.y
+    self.vx, self.vy
+    self.disk_colls, self.wall_colls
+
+
+
+
+class System:
+    self.time = 0
+    self.minpq  = [] # Min prority queue  //binary heap
+    self.particles
+
+# CAMBIAR POR MATHPLOTLIB
+    self.turtles
+    self.frame, self.fps
+
+class Event:
+    self.disk_a, self.disk_b
+    self.time
+    #self.criteria...
+
+
+class Animation:
+    self.System
+    #self.fig...
+    # atributos cómo antes
